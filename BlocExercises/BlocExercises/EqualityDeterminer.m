4b8955e734c32db6457c02cd499dd070712f1a72
Melissa <melbrng@gmail.com>
2015-09-11 13:07:53 -0600

    format code for readability

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -11,17 +11,26 @@
 @implementation EqualityDeterminer
 
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
+    
+    //check equality of string values
     BOOL areTheyEqual = [string1 isEqualToString:string2];
+    
     return areTheyEqual;
 }
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
+    
+    //check equality of numeric values
     BOOL areTheyEqual = [number1 isEqualToNumber:number2];
+    
     return areTheyEqual;
 }
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
-    BOOL areTheyGreater = integer1 > integer2;
+    
+    //check for greater value
+    BOOL areTheyGreater = ( integer1 > integer2 );
+    
     return areTheyGreater;
 }
 
