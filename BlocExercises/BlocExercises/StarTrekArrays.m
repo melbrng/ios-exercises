d2c219987e94e0d27094ce6a4881ba9b16b2cbe2
Melissa <melbrng@gmail.com>
2015-09-11 16:41:33 -0600

    completed assignment 13

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -11,23 +11,51 @@
 @implementation StarTrekArrays
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
-    /* WORK HERE */
-    return @[];
+
+    NSArray *characterArray = [characterString componentsSeparatedByString:@";"];
+    
+    return characterArray;
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @"";
+    
+    NSString *characterString = [characterArray componentsJoinedByString:@";"];
+    
+    return characterString;
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @[];
+    
+    //need a mutable copy to apply sortDescriptor
+    NSMutableArray *sortCharacterArray = [characterArray mutableCopy];
+    
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc]initWithKey:nil ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+    
+    [sortCharacterArray sortUsingDescriptors:@[sortDescriptor]];
+    
+    return sortCharacterArray;
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
-    /* WORK HERE */
-    return NO;
+  
+    //need mutable copy to apply predicate
+    NSMutableArray *worfCharacterArray = [characterArray mutableCopy];
+    
+    NSPredicate *containsWorf = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'Worf'"];
+    
+    [worfCharacterArray filterUsingPredicate:containsWorf];
+    
+    //if filtered instances of Worf
+    if(worfCharacterArray.count > 0)
+        
+    {
+        return YES;
+    }
+    
+    else
+    {
+        return NO;
+    }
 }
 
 @end
