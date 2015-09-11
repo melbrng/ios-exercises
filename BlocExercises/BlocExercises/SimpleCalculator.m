5449625faa7483ccdee383da53b6592a8643db6e
Melissa <melbrng@gmail.com>
2015-09-11 12:58:12 -0600

    format code for readability

--- BlocExercises/BlocExercises/SimpleCalculator.m
+++ BlocExercises/BlocExercises/SimpleCalculator.m
@@ -11,17 +11,23 @@
 @implementation SimpleCalculator
 
 - (NSInteger) increaseNumberBy1:(NSInteger) number {
-    NSInteger newInteger = number + 1;
+    
+    NSInteger newInteger = ( number + 1 );
+    
     return newInteger;
 }
 
 - (NSInteger) addNumber:(NSInteger) number1 toNumber:(NSInteger) number2 {
-    NSInteger newInteger = number1 + number2;
+    
+    NSInteger newInteger = ( number1 + number2 );
+    
     return newInteger;
 }
 
 - (NSInteger) remainderOfNumber:(NSInteger) dividend dividedByNumber:(NSInteger) divisor {
-    NSInteger newInteger = dividend % divisor;
+    
+    NSInteger newInteger = ( dividend % divisor );
+    
     return newInteger;
 }
 
